1f44744d916dacd8299d4775452885410adee338
upforde <upforde1@gmail.com>
2022-05-25 18:15:10 +0200

    changes to ftgpt2 ready to test

--- GPT-2/ftgpt2.py
+++ GPT-2/ftgpt2.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelWithLMHead, AutoTokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from transformers import TextDataset,DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 from transformers import pipeline
@@ -23,18 +23,18 @@ def load_dataset(train_path,test_path,tokenizer):
 class ftGPT2:
     def __init__(self, train_data, test_data):
         # Setting up the training datasets, tokenizer and model
-        tokenizer = AutoTokenizer.from_pretrained("gpt2")
-        train_dataset,test_dataset,data_collator = load_dataset(train_data,test_data,tokenizer)
-        self.model = AutoModelWithLMHead.from_pretrained("gpt2")
+        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        train_dataset,test_dataset,data_collator = load_dataset(train_data,test_data,self.tokenizer)
+        self.model = GPT2LMHeadModel.from_pretrained("gpt2")
 
         training_args = TrainingArguments(
             output_dir="./EMmodel",                                                     #The output directory
             overwrite_output_dir=True,                                                  #overwrite the content of the output directory
-            num_train_epochs=3,                                                         # number of training epochs
+            num_train_epochs=10,                                                        # number of training epochs
             per_device_train_batch_size=32,                                             # batch size for training
             per_device_eval_batch_size=32,                                              # batch size for evaluation
-            eval_steps = 400,                                                             # Number of update steps between two evaluations.
-            save_steps=800,                                                              # after # steps model is saved 
+            eval_steps = 400,                                                           # Number of update steps between two evaluations.
+            save_steps=800,                                                             # after # steps model is saved 
             warmup_steps=500,                                                           # number of warmup steps for learning rate scheduler
             prediction_loss_only=True,
             )
@@ -48,9 +48,13 @@ class ftGPT2:
             eval_dataset=test_dataset,
         )
 
-    def train(self):
         self.trainer.train()
+
+    def pipeline(self):
         return pipeline('text-generation', model=self.model, tokenizer='gpt2')
+
+    def getTokenizer(self):
+        return self.tokenizer
     
     def save(self):
         self.model.save_model()
--- GPT-2/main.py
+++ GPT-2/main.py
@@ -1,16 +1,19 @@
 import os, random
+
+from cv2 import FlannBasedMatcher
 from gpt2 import generator
 from ditto_parser import ditto_data_maker
+from ftgpt2 import ftGPT2
 
 save_location = "./Generated/er_magellan/Structured/Beer/"
 if not os.path.isdir(save_location): os.makedirs(save_location)
 
-train_matches = []
-with open("./Datasets/er_magellan/Structured/Beer/train.txt.matches") as file:
-  lines = file.readlines()
-  for line in lines:
-    arr = line.split("\t")
-    if "1" in arr[-1]: train_matches.append(line.strip())
+# train_matches = []
+# with open("./Datasets/er_magellan/Structured/Beer/train.txt.matches") as file:
+#   lines = file.readlines()
+#   for line in lines:
+#     arr = line.split("\t")
+#     if "1" in arr[-1]: train_matches.append(line.strip())
 
 # Getting validation set
 valid_matches = []
@@ -23,25 +26,38 @@ with open("./Datasets/er_magellan/Structured/Beer/valid.txt") as file:
 # Cutting the right half off from the validation set to make prompts
 cut_valid_matches = [item.split("\t")[0] + "\t" + item.split("\t")[1].split(" ")[0] for item in valid_matches]
 
-
 file = open(save_location + "fine_tuned_matches.txt", "a")
+
+# valid = False
+# while not valid:
+#   text = ""
+#   for i in range(5): 
+#     text += valid_matches[random.randint(0, len(valid_matches)-1)] + "\n"
+#   rand = cut_valid_matches[random.randint(0, len(cut_valid_matches)-1)]
+
+#   prompt = text + rand + "\tCOL"
+
+#   generated = generator(prompt, max_length=500, num_return_sequences=1)
+#   generated_text = generated[0]["generated_text"]
+#   print(generated_text)
+#   match = ditto_data_maker(generated_text)
+#   valid = match.isValid()
+  
+#   file.write(f"{valid}: {generated_text}")
+
+train_data = "./Datasets/er_magellan/Structured/Beer/train.txt.matches"
+test_data = "./Datasets/er_magellan/Structured/Beer/test.txt.matches"
+
+model = ftGPT2(train_data, test_data)
+tokenizer = model.getTokenizer()
+generator = model.pipeline()
 
 valid = False
 while not valid:
-  text = ""
-  for i in range(5): 
-    text += valid_matches[random.randint(0, len(valid_matches)-1)] + "\n"
-  rand = cut_valid_matches[random.randint(0, len(cut_valid_matches)-1)]
-
-  prompt = text + rand + "\tCOL"
-
-  generated = generator(prompt, max_length=500, num_return_sequences=1)
-  generated_text = generated[0]["generated_text"]
-  print(generated_text)
-  match = ditto_data_maker(generated_text)
+  text = cut_valid_matches[random.randint(0, len(cut_valid_matches)-1)]
+  generated = generator(text, max_length=round(len(tokenizer(text)['input_ids'])*3))[0]['generated_text']
+  match = ditto_data_maker(generated)
   valid = match.isValid()
-  
-  file.write(f"{valid}: {generated_text}")
 
 file.write(match.generate_string(1))
 file.write("\n")
