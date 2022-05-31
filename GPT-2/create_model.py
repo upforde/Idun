ae98fe901560ffb48e75596acb10d54bb2dbb581
upforde <upforde1@gmail.com>
2022-05-31 17:05:38 +0200

    small test

--- GPT-2/create_model.py
+++ GPT-2/create_model.py
@@ -31,48 +31,53 @@ if hp.decimate == "True":                                                       
     test_data += ".decimated"
     MODEL_NAME += "_decimated"
 
-# Function for creating the train-test datasets for the model
-def load_dataset(train_path,test_path,tokenizer):
-    train_dataset = TextDataset(
-        tokenizer=tokenizer,
-        file_path=train_path,
-        block_size=32)
+
+with open(train_data) as file:
+    for line in file.readlines():
+        print(line)
+
+# # Function for creating the train-test datasets for the model
+# def load_dataset(train_path,test_path,tokenizer):
+#     train_dataset = TextDataset(
+#         tokenizer=tokenizer,
+#         file_path=train_path,
+#         block_size=32)
     
-    test_dataset = TextDataset(
-        tokenizer=tokenizer,
-        file_path=test_path,
-        block_size=32)   
+#     test_dataset = TextDataset(
+#         tokenizer=tokenizer,
+#         file_path=test_path,
+#         block_size=32)   
     
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer, mlm=False,
-    )
-    return train_dataset,test_dataset,data_collator
+#     data_collator = DataCollatorForLanguageModeling(
+#         tokenizer=tokenizer, mlm=False,
+#     )
+#     return train_dataset,test_dataset,data_collator
 
-# Setting up the training datasets, tokenizer, model and trainer
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-train_dataset,test_dataset,data_collator = load_dataset(train_data,test_data,tokenizer)
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+# # Setting up the training datasets, tokenizer, model and trainer
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# train_dataset,test_dataset,data_collator = load_dataset(train_data,test_data,tokenizer)
+# model = GPT2LMHeadModel.from_pretrained("gpt2")
 
-training_args = TrainingArguments(
-    output_dir=MODEL_NAME,                                                      # The output directory
-    overwrite_output_dir=True,                                                  # overwrite the content of the output directory
-    num_train_epochs=5,                                                         # number of training epochs
-    per_device_train_batch_size=32,                                             # batch size for training
-    per_device_eval_batch_size=32,                                              # batch size for evaluation
-    eval_steps = 400,                                                           # Number of update steps between two evaluations.
-    save_steps=800,                                                             # after # steps model is saved 
-    warmup_steps=500,                                                           # number of warmup steps for learning rate scheduler
-    prediction_loss_only=True,
-    )
+# training_args = TrainingArguments(
+#     output_dir=MODEL_NAME,                                                      # The output directory
+#     overwrite_output_dir=True,                                                  # overwrite the content of the output directory
+#     num_train_epochs=5,                                                         # number of training epochs
+#     per_device_train_batch_size=32,                                             # batch size for training
+#     per_device_eval_batch_size=32,                                              # batch size for evaluation
+#     eval_steps = 400,                                                           # Number of update steps between two evaluations.
+#     save_steps=800,                                                             # after # steps model is saved 
+#     warmup_steps=500,                                                           # number of warmup steps for learning rate scheduler
+#     prediction_loss_only=True,
+#     )
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=train_dataset,
-    eval_dataset=test_dataset,
-)
+# trainer = Trainer(
+#     model=model,
+#     args=training_args,
+#     data_collator=data_collator,
+#     train_dataset=train_dataset,
+#     eval_dataset=test_dataset,
+# )
 
-# Training and saving the model
-trainer.train()
-trainer.save_model()
+# # Training and saving the model
+# trainer.train()
+# trainer.save_model()
