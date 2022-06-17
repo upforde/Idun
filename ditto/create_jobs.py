a65c7f875847e6afba50f3444e1b0e5bb896cafe
upforde <upforde1@gmail.com>
2022-06-17 22:04:05 +0200

    installing requirement

--- ditto/create_jobs.py
+++ ditto/create_jobs.py
@@ -32,6 +32,19 @@ def make_text(task, name):
         "#SBATCH --mail-type=ALL",
         "module purge",
         "module load Anaconda3/2020.07",
+        "pip3 install gensim==3.8.1 --user",
+        "pip3 install numpy==1.19.2 --user",
+        "pip3 install regex==2019.12.20 --user",
+        "pip3 install scipy==1.3.2 --user",
+        "pip3 install sentencepiece==0.1.85 --user",
+        "pip3 install sklearn==0.0 --user",
+        "pip3 install spacy==3.1 --user",
+        "pip3 install torch==1.9.0+cu111 --user",
+        "pip3 install transformers==4.9.2 --user",
+        "pip3 install tqdm==4.41.0 --user",
+        "pip3 install jsonlines==1.2.0 --user",
+        "pip3 install nltk==3.5 --user",
+
         python_line,
         "uname -a"
     ]
