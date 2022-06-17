24cd9377729c7ecc706d186ed4671cc83e115be4
upforde <upforde1@gmail.com>
2022-06-17 22:15:50 +0200

    module apex

--- ditto/create_jobs.py
+++ ditto/create_jobs.py
@@ -39,12 +39,13 @@ def make_text(task, name):
         "pip3 install sentencepiece==0.1.85 --user",
         "pip3 install sklearn==0.0 --user",
         "pip3 install spacy==3.1 --user",
-        "pip3 install torch==1.9.0+cu111 --user",
+        "pip3 install torch==1.9.0 --user",
         "pip3 install transformers==4.9.2 --user",
         "pip3 install tqdm==4.41.0 --user",
         "pip3 install jsonlines==1.2.0 --user",
         "pip3 install nltk==3.5 --user",
         "pip3 install tensorboardX --user",
+        "pip3 install apex --user",
 
         python_line,
         "uname -a"
