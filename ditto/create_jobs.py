1a46fed3cf43cc33c275406e1de97080e86404eb
upforde <upforde1@gmail.com>
2022-06-18 19:44:44 +0200

    seed = run_id

--- ditto/create_jobs.py
+++ ditto/create_jobs.py
@@ -14,8 +14,8 @@ def make_text(task, name):
     python_line += f" --task={task}"
     python_line += " --batch_size=32"
     python_line += f" --output_name=Output/{name}"
-    seed = str(round(random.random()*214748364))
-    python_line += f" --seed={seed}"
+    seed = str(round(random.random()*2147483647))
+    python_line += f" --run_id={seed}"
 
     text = [
         "#!/bin/sh",
