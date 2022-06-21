eb3ec170bd19c3cd6aaaa443957112067c969032
upforde <upforde1@gmail.com>
2022-06-21 19:15:49 +0200

    uninstall apex

--- GPT-2/cg_jobs.py
+++ GPT-2/cg_jobs.py
@@ -32,9 +32,7 @@ def make_text(output, dataset, entity_type, decimate):
         "module purge",
         "module load Anaconda3/2020.07",
         "pip3 install transformers==4.2.2 --user",
-        "cd apex",
-        "pip3 install --user -v --disable-pip-version-check --no-cache-dir --global-option=\"--cpp_ext\" --global-option=\"--cuda_ext\" ./",
-        "cd ..",
+        "pip3 uninstall apex",
         python_line,
         "uname -a"
     ]
