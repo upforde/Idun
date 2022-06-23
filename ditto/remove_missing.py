3ad17b982ea88db39b1ae929d9a41cbea9f62086
upforde <upforde1@gmail.com>
2022-06-23 18:22:38 +0200

    slight changes

--- ditto/remove_missing.py
+++ ditto/remove_missing.py
@@ -1,24 +1,27 @@
 new_gpt2_ft = []
 new_gpt2_nft = []
 
-with open("./run_missing.sh") as missing:
-    missing_lines = missing.readlines()
-    with open("./run_gpt2_ft_jobs.sh") as gpt2:
-        for line in gpt2.readlines():
-            if line not in missing_lines:
-                new_gpt2_ft.append(line)
-    with open("./run_gpt2_nft_jobs.sh") as gpt2:
-        for line in gpt2.readlines():
-            if line not in missing_lines:
-                new_gpt2_nft.append(line)
+missing = open("./run_missing.sh").readlines()
+
+with open("./run_gpt2_ft_jobs.sh") as gpt2:
+    for line in gpt2.readlines():
+        if line not in missing:
+            new_gpt2_ft.append(line)
+
+with open("./run_gpt2_nft_jobs.sh") as gpt2:
+    for line in gpt2.readlines():
+        if line not in missing:
+            new_gpt2_nft.append(line)
 
 
 open("./run_gpt2_ft_jobs.sh", "w").close()
+with open("./run_gpt2_ft_jobs.sh", "a")as ft: ft.write("#!/bin/sh")
 with open("./run_gpt2_ft_jobs.sh", "a") as gpt2:
     for line in new_gpt2_ft:
-        gpt2.write(line + "\n")
+        gpt2.write(line)
 
 open("./run_gpt2_nft_jobs.sh", "w").close()
+with open("./run_gpt2_nft_jobs.sh", "a")as nft: nft.write("#!/bin/sh")
 with open("./run_gpt2_nft_jobs.sh", "a") as gpt2:
-    for line in new_gpt2_ft:
-        gpt2.write(line + "\n")
+    for line in new_gpt2_nft:
+        gpt2.write(line)
